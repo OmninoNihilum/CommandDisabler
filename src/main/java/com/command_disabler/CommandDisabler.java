15e3596472121d68671e06a1b1a4ea53a8dd6e97
Nihilum <omninonihilum@gmail.com>
2026-03-22 15:16:24 +0200

    fix hidden commands

--- src/main/java/com/command_disabler/CommandDisabler.java
+++ src/main/java/com/command_disabler/CommandDisabler.java
@@ -2,10 +2,12 @@ package com.command_disabler;
 
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.CommandEvent;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 
 @Mod("command_disabler")
@@ -13,18 +15,23 @@ public class CommandDisabler {
     public CommandDisabler(IEventBus modEventBus) {
         NeoForge.EVENT_BUS.register(this);
     }
-
     @SubscribeEvent
     public void onRegisterCommands(RegisterCommandsEvent event) {
         CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
 
-        removeCommand(dispatcher, "msg");
-        removeCommand(dispatcher, "tell");
-        removeCommand(dispatcher, "teammsg");
-        removeCommand(dispatcher, "tm");
+        disable(dispatcher, "msg");
+        disable(dispatcher, "tell");
+        disable(dispatcher, "w");
+        disable(dispatcher, "teammsg");
+        disable(dispatcher, "tm");
     }
 
-    private void removeCommand(CommandDispatcher<CommandSourceStack> dispatcher, String name) {
+    private void disable(CommandDispatcher<CommandSourceStack> dispatcher, String name) {
         dispatcher.getRoot().getChildren().removeIf(node -> node.getName().equals(name));
+
+        dispatcher.register(
+                Commands.literal(name)
+                        .requires(source -> false)
+        );
     }
 }
